ac78fdfa27e391981a1ac8060fddf0eb8578bb67
Chinnell Davis <chinnell.davis@gmail.com>
2017-01-08 15:39:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/* global $ */
+$(document).ready(function(){
+    //This page is for JavaScript code including jQuery. Use the comments below to complete activities 1 through 4.
+    
+    // 1. On line 7 use jQuery to change the text color of the h2 tags.
+    $("#button1").click(function() {
+        alert("hi");
+    });
+    
+    
+    // 2. On line 13 use jQuery to append "I live in NYC" to the h2 tag with the id four.
+    $("#button2").click(function() {
+
+    });
+    
+    // 3a. Write a click function for the button with id button3.
+    // 3b. Inside your button3 click function create an alert that asks the user "Are you sure you want to click this button?"
+    
+      
+});

37db3f87cc5eeff157100317c4c2c358152cc301
niya williams <niyawilliams4514@gmail.com>
2017-01-23 15:10:31 +0000

    I changed likns in about.html and contact.html

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
     
     // 1. On line 7 use jQuery to change the text color of the h2 tags.
     $("#button1").click(function() {
-        alert("hi");
+        $("#three").fadeOut("slow");
+            alert("hi");
     });
     
     
     // 2. On line 13 use jQuery to append "I live in NYC" to the h2 tag with the id four.
     $("#button2").click(function() {
-
+$
     });
     
     // 3a. Write a click function for the button with id button3.
